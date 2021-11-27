98d58c516d61ef70f791f96fe5efc6e0547a4fc5
Vasil Hristov <hristov_vasil@yahoo.com>
2021-11-27 11:09:44 +0200

    fix: add btn tooltip

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-const result = document.querySelector('.password-result')
-const copy = document.querySelector('.copy-btn')
-const generate = document.querySelector('.generate-btn')
-const form = document.querySelector('form')
-const pLength = document.querySelector('#passwordLength')
-const pSpacing = document.querySelector('#passwordSpacing')
-const uppercaseSwitch = document.querySelector('#uppercaseSwitch')
-const lowercaseSwitch = document.querySelector('#lowercaseSwitch')
-const numbersSwitch = document.querySelector('#numbersSwitch')
-const symbolsSwitch = document.querySelector('#symbolsSwitch')
+const getDomElements = () => {
+    return {
+        root: document.documentElement,
+        result: document.querySelector('.password-result'),
+        copy: document.querySelector('.copy-btn'),
+        generate: document.querySelector('.generate-btn'),
+        form: document.querySelector('form'),
+        pLength: document.querySelector('#passwordLength'),
+        pSpacing: document.querySelector('#passwordSpacing'),
+        uppercaseSwitch: document.querySelector('#uppercaseSwitch'),
+        lowercaseSwitch: document.querySelector('#lowercaseSwitch'),
+        numbersSwitch: document.querySelector('#numbersSwitch'),
+        symbolsSwitch: document.querySelector('#symbolsSwitch'), 
+        tooltipText: document.querySelector('.custom-tooltiptext')
+    }
+}
 
-copy.addEventListener('click', copyToClipboard)
-
-form.addEventListener('submit', (event) => {
+const onSubmitForm = (event) => {
     event.preventDefault()
+
+    const { pLength, pSpacing, uppercaseSwitch, lowercaseSwitch, numbersSwitch, symbolsSwitch, result } = getDomElements()
 
     const args = {
         len: Number(pLength.value),
@@ -26,9 +32,9 @@ form.addEventListener('submit', (event) => {
     const password = passwordGenerator(args)
 
     result.textContent = password
-})
+}
 
-function passwordGenerator({ len, spacing, isUppercase, isLowercase, hasNumbers, hasSymbols }) {
+const passwordGenerator = ({ len, spacing, isUppercase, isLowercase, hasNumbers, hasSymbols }) => {
     const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz'
     const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     const numbers = '0123456789'
@@ -45,11 +51,49 @@ function passwordGenerator({ len, spacing, isUppercase, isLowercase, hasNumbers,
     return blank.map((_, idx) => idx % (spacing + 1) == spacing ? '-' : set.charAt(Math.floor(Math.random() * set.length))).join('')
 }
 
-function copyToClipboard () {
+const copyToClipboard = (event) => {
+    const { generate, result } = getDomElements()
+
     if (result.textContent) {
         navigator.clipboard.writeText(result.textContent);
+
+        changeCopyBtnText(event)
         return
     }
 
     generate.focus()
 }
+
+const changeCopyBtnText = (event) => {
+    const { root, tooltipText } = getDomElements()
+
+    const props = {
+        mouseover: {
+            'text': 'Copy to clipboard',
+            '--copy-btn-width': '120px',
+            '--copy-btn-margin-right': '-60px'
+        },
+        click: {
+            'text': 'Copied!',
+            '--copy-btn-width': '60px',
+            '--copy-btn-margin-right': '-30px'
+        }
+    }
+
+    root.style.setProperty('--copy-btn-width', props[event.type]['--copy-btn-width']);
+    root.style.setProperty('--copy-btn-margin-right', props[event.type]['--copy-btn-margin-right']);
+    tooltipText.textContent = props[event.type].text
+}
+
+// Initialize function
+const init = () => {
+    const { copy, form } = getDomElements()
+
+    copy.addEventListener('click', copyToClipboard)
+
+    copy.addEventListener("mouseover", changeCopyBtnText)
+
+    form.addEventListener('submit', onSubmitForm)
+}
+
+init()
